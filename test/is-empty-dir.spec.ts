2cc54c95b9e0ecd10d8e288bf136c95797fa6ec6
3imed-jaberi <imed_jebari@hotmail.fr>
2021-08-16 01:35:01 +0200

    add is-empty-dir test 🧪 ..

---
+++ test/is-empty-dir.spec.ts
@@ -0,0 +1,21 @@
+import assert from 'assert'
+
+import { mkdirpSync } from '../src/mkdir'
+import { rm } from '../src/rm'
+import { isEmptyDir, isEmptyDirSync } from '../src/is-empty-dir'
+
+describe('is-dir', () => {
+  it('async', async () => {
+    assert(!await isEmptyDir('test'))
+    mkdirpSync('empty')
+    assert(await isEmptyDir('empty'))
+    await rm('empty')
+  })
+
+  it('sync', async () => {
+    assert(!isEmptyDirSync('test'))
+    mkdirpSync('empty')
+    assert(isEmptyDirSync('empty'))
+    await rm('empty')
+  })
+})
