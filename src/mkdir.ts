a0d7d2902dfc04b1f269be82317ef0943c08d9d4
3imed-jaberi <imed_jebari@hotmail.fr>
2021-08-16 01:52:22 +0200

    add mkdir-adapt code 🚀 ..

--- src/mkdir.ts
+++ src/mkdir.ts
@@ -1,12 +1,15 @@
 import fs from 'fs'
 import type { PathLike, MakeDirectoryOptions } from 'fs'
 
-export async function mkdirp(path: PathLike, options?: MakeDirectoryOptions) {
-  const direcotyPath = await fs.promises.mkdir(path, { recursive: true, ...options })
-  return direcotyPath
+export async function mkdirp(path: PathLike|PathLike[], options?: MakeDirectoryOptions) {
+  if (!Array.isArray(path)) path = [path]
+  const pathsPromises = path.map(p => fs.promises.mkdir(p, { recursive: true, ...options }))
+  const direcotyPaths = await Promise.all(pathsPromises)
+  return direcotyPaths
 }
 
-export function mkdirpSync(path: PathLike, options?: MakeDirectoryOptions) {
-  const direcotyPath = fs.mkdirSync(path, { recursive: true, ...options })
-  return direcotyPath
+export function mkdirpSync(path: PathLike|PathLike[], options?: MakeDirectoryOptions) {
+  if (!Array.isArray(path)) path = [path]
+  const direcotyPaths = path.map(p => fs.mkdirSync(p, { recursive: true, ...options }))
+  return direcotyPaths
 }
