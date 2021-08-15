3a182ecce753dd3b9f9bacac455b4e3ab09ec490
3imed-jaberi <imed_jebari@hotmail.fr>
2021-08-16 00:23:37 +0200

    fix tests --pass 🧪 ..

--- test/is-path-exist.spec.ts
+++ test/is-path-exist.spec.ts
@@ -4,12 +4,12 @@ import { isPathExist, isPathExistSync } from '../src/is-path-exist'
 
 describe('is-path-exist', () => {
   it('async', async () => {
-    assert(await isPathExist('is-path-exist.spec.ts'))
+    assert(await isPathExist('LICENSE'))
     assert(!(await isPathExist('failed')))
   })
 
-  it('async', async () => {
-    assert(isPathExistSync('is-path-exist.spec.ts'))
+  it('sync', async () => {
+    assert(isPathExistSync('LICENSE'))
     assert(!isPathExistSync('failed'))
   })
 })
