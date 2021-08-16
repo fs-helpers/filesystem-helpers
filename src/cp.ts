2b593a5d6d2f1c34ce693cd3e773f18ad46af073
3imed-jaberi <imed_jebari@hotmail.fr>
2021-08-16 03:27:50 +0200

    add sync support to cp --experimental 🐱‍🐉

--- src/cp.ts
+++ src/cp.ts
@@ -1,7 +1,7 @@
-import fs, { ReadStream } from 'fs'
+import fs, { ReadStream, WriteStream } from 'fs'
 import path from 'path'
 import destroy from 'destroy'
-import { mkdirp } from './mkdir'
+import { mkdirp, mkdirpSync } from './mkdir'
 import { randomStr } from './temp-path'
 
 export async function cp(src: string|ReadStream, dest: string) {
@@ -38,13 +38,55 @@ export async function cp(src: string|ReadStream, dest: string) {
   
     await fs.promises.rename(tmp, dest)
     
-    return dest;
+    return dest
   } catch (error) {
     // always destroy the streams
-    read && destroy(read)
-    write && destroy(write)
+    destroy(read)
+    destroy(write)
     // always clean up temp files
     await fs.promises.unlink(tmp).catch(() => {}) // eslint-disable-line @typescript-eslint/no-empty-function
     throw error
   }
+}
+
+
+export function cpSync(src: string|ReadStream, dest: string) {
+  let write: WriteStream
+  let read: ReadStream
+  let tmp
+
+  read = (src instanceof ReadStream) ? src : fs.createReadStream(src)
+
+  dest = path.resolve(dest)
+  // where the file will be temporarily copied to
+  // it'll be saved to the same folder, then renamed.
+  // (to avoid race conditions ...)
+  tmp = `${dest}.${randomStr()}.tmp`
+  
+  mkdirpSync(path.dirname(dest))
+
+  // processing
+  read.on('error', onfinish)
+  write = fs.createWriteStream(tmp)
+  write.on('error', onfinish)
+  write.on('close', onfinish)
+  read.pipe(write)
+
+  function onfinish(err) {
+    read.removeListener('error', onfinish)
+    write.removeListener('error', onfinish)
+    write.removeListener('close', onfinish)
+    
+    // always destroy the streams
+    destroy(read)
+    destroy(write)
+    // always clean up temp files
+    fs.unlinkSync(tmp)
+
+    if (err instanceof Error) throw err
+    return dest
+  }
+
+  fs.renameSync(tmp, dest)
+  return dest
 }
